20697eaf019bcd1e4b09d136bac5f989329e8d7e
Heber <sengerhe@gmail.com>
2023-05-05 10:08:25 -0300

    add await

--- deferred-link.js
+++ deferred-link.js
@@ -2,8 +2,8 @@
 const pseudoToken =
   "company:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
-function cta(copyToken) {
-  navigator.clipboard.writeText(copyToken);
+async function cta(copyToken) {
+  await navigator.clipboard.writeText(copyToken);
   window.location.href =
     "https://apps.apple.com/br/app/f1-tv/id1315007279?l=en";
 }
