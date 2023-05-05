0eb439f86be501e1774e7309fce1887d044f0d95
Heber <sengerhe@gmail.com>
2023-05-05 10:22:17 -0300

    info

--- deferred-link.js
+++ deferred-link.js
@@ -1,8 +1,8 @@
 // JWT token from https://jwt.io/
-const pseudoToken =
-  "company:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
-
 async function cta() {
+  const pseudoToken =
+    "company:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
   await navigator.clipboard.writeText(pseudoToken);
   window.location.href =
     "https://apps.apple.com/br/app/f1-tv/id1315007279?l=en";
