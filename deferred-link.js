c57f82442d67c929cea8c5efc91681cf742e97f1
Heber <sengerhe@gmail.com>
2023-05-05 08:58:12 -0300

    basic functionality

---
+++ deferred-link.js
@@ -0,0 +1,7 @@
+// JWT token from https://jwt.io/
+const pseudoToken =
+  "company:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+function cta(copyToken) {
+  navigator.clipboard.writeText(copyToken);
+}
